d7d08382012badf94eacd7d80955f4f74c1100ef
Unknown <coreankim@gmail.com>
2018-01-12 09:47:35 -0500

    Version with udpdated SQL seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,9 +5,9 @@ INSERT INTO scenarios (
   )
   VALUES
     ("Default", "", ""),
-    ("Femur", "Proximal", "Fix it!"),
-    ("Femur", "Midshaft", "Fix it!"),
-    ("Femur", "Distal", "Fix it!");
+    ("Femur", "Proximal", "AP pelvis, lateral hip and Judet views (both pre and post-reduction) || inlet and outlet views (if acetabular or pelvic ring injury suspected) || CT after reduction || Get consent || Pre-operative medicine clearance"),
+    ("Femur", "Midshaft", "AP/lateral of entire femur || AP/lateral of ipsilateral hip || AP/lateral of ipsilateral knee || Call attending on staff || Get consent || Pre-operative medicine clearance"),
+    ("Femur", "Distal", "Standard AP/Lateral || Traction AP/Lateral/oblique || Call attending on staff || Get consent || Pre-operative medicine clearance!");
 
 INSERT INTO patients ( 
   patient_name,
