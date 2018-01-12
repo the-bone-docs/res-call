0cf4828be5c8200d4250d5a8355788a61b922592
Wenbiao Li <lixuyao.tony@gmail.com>
2018-01-12 02:07:22 -0500

    init commit

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-  var connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-  user: "root",
-  password: "root",
-  database: "resCallDB",
-  port: "8889"
-  });
-};
+var connection = mysql.createConnection("");
+
+// if (process.env.JAWSDB_URL) {
+//   var connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+//   user: "root",
+//   password: "root",
+//   database: "resCallDB",
+//   port: "8889"
+//   });
+// };
 
 // Make connection.
 connection.connect(function(err) {
